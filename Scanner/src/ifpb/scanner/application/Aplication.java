57b9ce9b7360a6b8ba228557ea33d6f4cc3d37c8
Alex <Alex@192.168.25.12>
2014-05-24 23:17:16 -0300

    Commit test

--- Scanner/src/ifpb/scanner/application/Aplication.java
+++ Scanner/src/ifpb/scanner/application/Aplication.java
@@ -5,7 +5,7 @@ import exceptions.NotGetDeviceException;
 import ifpb.scanner.controller.Sistema;
 
 public class Aplication {
-
+    //Teste
 	public static void main(String[] args) {
 		Sistema system = new Sistema();
 		try {
